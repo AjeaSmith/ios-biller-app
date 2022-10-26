64bc32746156bf4e154eb87b87f1502c45fe97c7
ajea <smithajea@rocketmail.com>
2022-10-26 04:30:54 -0400

    Initial Commit

---
+++ Biller/BillerApp.swift
@@ -0,0 +1,20 @@
+//
+//  BillerApp.swift
+//  Biller
+//
+//  Created by Ajea Smith on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BillerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
