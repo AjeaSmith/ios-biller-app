c4e55846ecd5cd216b7c995999084b15e578a28a
ajea <smithajea@rocketmail.com>
2022-10-31 02:10:05 -0400

    - add fonts for landing and listing views - add colors and image used for landing and listing views - refactor folder structure to MVVM - design landing page view

---
+++ Biller/Helpers/Fonts.swift
@@ -0,0 +1,21 @@
+//
+//  Fonts.swift
+//  Biller
+//
+//  Created by Ajea Smith on 10/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    public static var landingText: Font {
+        return Font.custom("Lora-Medium", size: 14)
+    }
+    public static var listingText1: Font {
+        return Font.custom("Lora-Regular", size: 16)
+    }
+    public static var listingText2: Font {
+        return Font.custom("Lora-Regular", size: 12)
+    }
+}
---
+++ Biller/Views/LandingPageView.swift
@@ -0,0 +1,64 @@
+//
+//  LandingPageView.swift
+//  Biller
+//
+//  Created by Ajea Smith on 10/30/22.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    let persistenceController = PersistenceController.shared
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color("landing-background")
+                VStack(spacing: 50){
+                    Image(systemName: "dollarsign.circle")
+                        .font(.system(size: 100))
+                        .foregroundColor(Color("landing-text"))
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor")
+                        .font(Font.landingText)
+                        .foregroundColor(Color("landing-text"))
+                        .frame(width:240)
+                        .multilineTextAlignment(.center)
+                    
+                    NavigationLink {
+                        RootView().environment(\.managedObjectContext, persistenceController.container.viewContext)
+                    } label: {
+                        ZStack{
+                            Capsule()
+                                .frame(width: 189, height: 50)
+                                .foregroundColor(Color("landing-button"))
+                                .shadow(color: Color("shadow"), radius: 4, x: 0, y: 4)
+                            HStack{
+                                Spacer()
+                                Text("Get Started")
+                                    .font(Font.landingText)
+                                    .foregroundColor(Color("landing-button-text"))
+                                    .padding(.trailing, 5)
+                                    .navigationBarHidden(true)
+                                    .navigationTitle("")
+
+                                Image(systemName: "arrow.right")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(Color("landing-background"))
+//                                Image("landing-arrow")
+//                                    .frame(width: 14, height: 14)
+                                Spacer()
+                            }
+                        }
+                    }
+                }
+
+            }.ignoresSafeArea()
+        }
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
+}
