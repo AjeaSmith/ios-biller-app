a0ec71a8a8678df31db5639005b75f3562dd9f96
ajea <smithajea@rocketmail.com>
2022-11-07 09:18:15 -0500

    - removed print statement

--- Biller/Views/tabview/TabBarView.swift
+++ Biller/Views/tabview/TabBarView.swift
@@ -34,7 +34,6 @@ struct TabBarView: View {
                     }
                     .onAppear(perform: {
                         calendar.reloadData()
-                        print("hello")
                     })
                 
                 SettingsView()
